7684bf3705d117a27d6daba9e0d9343d07d942f6
Somebody-hub <119055425+Somebody-hub@users.noreply.github.com>
2022-11-29 22:16:04 +0300

    Task 2: Numbers

--- tasks.php
+++ tasks.php
@@ -5,3 +5,17 @@ $order = $very_bad_unclear_name;
 $order .= "and some spicy sauce";
 echo $very_bad_unclear_name . "\n";
 // end task
+
+// task2: Numbers
+$var = 4;
+echo $var . "\n";
+
+$float_var = 490.5;
+echo "float variable = " . $float_var . "\n";
+
+echo 12 . "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "the difference in spending money = " . ($last_month - $this_month) . "\n";
+// end task
