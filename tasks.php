abac5f50c95359ffde59c4b917deca6e64dc6a84
Somebody-hub <119055425+Somebody-hub@users.noreply.github.com>
2022-11-29 22:12:28 +0300

    task 1: Link acces

--- tasks.php
+++ tasks.php
@@ -1,1 +1,7 @@
-
+<?php
+// task1: Link access
+$very_bad_unclear_name = "15 chicken wings";
+$order = $very_bad_unclear_name;
+$order .= "and some spicy sauce";
+echo $very_bad_unclear_name . "\n";
+// end task
